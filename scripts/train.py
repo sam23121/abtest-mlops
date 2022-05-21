2d969e70d08e34aae1d045d11ec225b4a6426898
Samuel Alene <smlalene@gmail.com>
2022-05-21 13:14:43 -0700

    adding graph for cml report

--- scripts/train.py
+++ scripts/train.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.abspath(os.path.join('../scripts')))
 import pandas as pd
 import numpy as np
 import seaborn as sns
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, confusion_matrix, accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
